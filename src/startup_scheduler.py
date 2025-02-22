0d0b02cb56b802cf17d4e01e0b77c08264199430
Paul Goddard <goddard.paul@gmail.com>
2025-02-22 09:24:31 +0000

    added logic to prevent updating between two times

--- src/startup_scheduler.py
+++ src/startup_scheduler.py
@@ -7,8 +7,24 @@ def sync_rtc_time():
 	now = datetime.datetime.now()
 	witty.set_rtc_datetime(now)
 
+def is_time_to_sleep(time):
+	no_updates_from = datetime.datetime.strptime(config.current.no_updates_from, "%H:%M").time()
+	no_updates_to = datetime.datetime.strptime(config.current.no_updates_to, "%H:%M").time()
+
+	# Check if the 'no_updates_to' time is earlier than 'no_updates_from' (i.e., crosses midnight)
+	if no_updates_to < no_updates_from:
+		# If so, consider the 'no_updates_to' as being on the next day
+		if no_updates_from <= time.time() or time.time() <= no_updates_to:
+			return True
+	else:
+		# Normal case: no crossing midnight
+		if no_updates_from <= time.time() <= no_updates_to:
+			return True
+		
+	return False
+
 def schedule_next_startup():
-	now = datetime.datetime.now()
+	now = datetime.datetime.now()	
 
 	if config.current.refresh_mode == "hourly":
 		time = now + datetime.timedelta(hours = 1)
@@ -27,6 +43,18 @@ def schedule_next_startup():
 			minute = int(split[1]), 
 			second = 0
 		)
+	
+	# check if the new time is between the no_updates_from and no_updates_to
+	# times in the config file. If the new time is, override the time with
+	# the no_updates_to time
+	if(is_time_to_sleep(time)):
+		split = config.current.no_updates_to.split(":")
+		time = time.replace(
+			hour = int(split[0]), 
+			minute = int(split[1]), 
+			second = 0
+		)
+
 	witty.set_startup_time(time)
 	print("Scheduled startup to " + witty.get_startup_time())
 
